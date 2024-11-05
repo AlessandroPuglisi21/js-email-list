7d05a7189587bbbc85199a9a41899015269ceab6
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-05 15:54:41 +0100

    UPDATE: added a function for generating emails

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const numberOfMail = 10
 let randomMail = [];
 const emailList = document.getElementById('emailList')
 
+function generateRandomEmails(){
+
+    let randomMail = [];
+
 for(let i = 0; i < numberOfMail; i++){
     axios 
     .get('https://flynn.boolean.careers/exercises/api/random/mail')
@@ -12,9 +16,11 @@ for(let i = 0; i < numberOfMail; i++){
         const listItem = document.createElement('ol');
         listItem.textContent = email;
         emailList.appendChild(listItem)
-    })
+ })
     .catch(err => {
         console.log('Errore durante la richiesta', err)
     })
 }
-console.log(randomMail)
+}
+
+generateRandomEmails();
