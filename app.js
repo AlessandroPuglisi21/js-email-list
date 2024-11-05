349695e16874b1c467c4c95e2d6a08b1cd1a0b9a
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-05 15:37:54 +0100

    ADD:Email generation on Screen

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const numberOfMail = 10
 let randomMail = [];
+const emailList = document.getElementById('emailList')
 
 for(let i = 0; i < numberOfMail; i++){
     axios 
@@ -7,6 +8,13 @@ for(let i = 0; i < numberOfMail; i++){
     .then(res => {
         const email = res.data.response;
         randomMail.push(email)
+
+        const listItem = document.createElement('ol');
+        listItem.textContent = email;
+        emailList.appendChild(listItem)
+    })
+    .catch(err => {
+        console.log('Errore durante la richiesta', err)
     })
 }
 console.log(randomMail)
